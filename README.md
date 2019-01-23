ad681176a834bf79ecd3fa32af1d2050927c0c58
Piotr Kożuchowski <p.kozuchowski@polsource.com>
2019-01-23 11:04:52 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# salesforceTools
+Tools for programmatically managing Salesforce metadata (currently only Profiles).
+
+    String profileDirectory = "...\\force-app\\main\\default\\profiles\\";
+    String[] profilePaths = new String[]{
+            profileDirectory + "Admin.profile-meta.xml",
+            profileDirectory + "Sales.profile-meta.xml",
+            profileDirectory + "Service.profile-meta.xml",
+    };
+    
+    for (String profilePath : profilePaths) {
+        Profile profile = new Profile(profilePath);
+        
+        profile.add(new FieldPermission("Account.Custom_Field_1__c", true, false));
+        profile.add(new FieldPermission("Account.Custom_Field_2__c", true, true));
+        profile.remove(new FieldPermission("Account.Custom_Field_2__c"));
+    
+        profile.add(new ObjectPermission("Custom_Object__c", true, false, true, true, false, false));
+        profile.add(new TabVisibility("Application_Log__c", TabVisibility.DefaultOn));
+        profile.saveFile();
+    }
--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,21 @@ import sfdc.profiles.profileNodes.*;
 
 public class Main {
     public static void main(String[] args) {
-        String profileDirectory = "C:\\Users\\piotr\\IdeaProjects\\goodenergy\\force-app\\main\\default\\profiles\\";
+        String profileDirectory = "...\\force-app\\main\\default\\profiles\\";
         String[] profilePaths = new String[]{
                 profileDirectory + "Admin.profile-meta.xml",
-                profileDirectory + "Business Intelligence User.profile-meta.xml",
-                profileDirectory + "Customer Operations.profile-meta.xml",
-                profileDirectory + "Customer Operations %28Quoting%29.profile-meta.xml",
-                profileDirectory + "Customer Service.profile-meta.xml",
-                profileDirectory + "DST Sales.profile-meta.xml",
-                profileDirectory + "Energy Solution Sales.profile-meta.xml",
-                profileDirectory + "Enterprise Sales.profile-meta.xml",
-                profileDirectory + "Integration User.profile-meta.xml",
-                profileDirectory + "MarketingProfile.profile-meta.xml",
-                profileDirectory + "PolSource Admin.profile-meta.xml",
-                profileDirectory + "ReadOnly.profile-meta.xml",
-                profileDirectory + "SME Sales.profile-meta.xml"
+                profileDirectory + "Sales.profile-meta.xml",
+                profileDirectory + "Service.profile-meta.xml",
         };
 
         for (String profilePath : profilePaths) {
             Profile profile = new Profile(profilePath);
-            profile.add(new FieldPermission("Meter_Point__c.Generator_ROC_ID__c", true, false));
-            profile.add(new ObjectPermission("Meter_Point__c", true, false, true, true, false, false));
 
-            profile.remove(new FieldPermission("Meter_Point__c.Generator_ROC_ID__c"));
+            profile.add(new FieldPermission("Account.Custom_Field_1__c", true, false));
+            profile.add(new FieldPermission("Account.Custom_Field_2__c", true, true));
+            profile.remove(new FieldPermission("Account.Custom_Field_2__c"));
+
+            profile.add(new ObjectPermission("Custom_Object__c", true, false, true, true, false, false));
             profile.add(new TabVisibility("Application_Log__c", TabVisibility.DefaultOn));
             profile.saveFile();
         }
