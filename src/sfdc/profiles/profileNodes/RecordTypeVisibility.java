edb0777e3b9dbe6855bc49354f25e11ddf119d23
Piotr Kożuchowski <p.kozuchowski@polsource.com>
2020-10-05 09:51:25 +0200

    RecordTypeVisibility: Added personAccountDefault node

--- src/sfdc/profiles/profileNodes/RecordTypeVisibility.java
+++ src/sfdc/profiles/profileNodes/RecordTypeVisibility.java
@@ -2,13 +2,15 @@ package sfdc.profiles.profileNodes;
 
 import org.w3c.dom.Element;
 
+import java.util.Locale;
 import java.util.Map;
 
 public class RecordTypeVisibility extends ProfileNode {
     private String recordType;
     private Boolean
             isDefault,
-            visible;
+            visible,
+            personAccountDefault;
 
     protected RecordTypeVisibility() {
     }
@@ -28,13 +30,19 @@ public class RecordTypeVisibility extends ProfileNode {
         this.recordType = nodeValues.get("recordType");
         this.isDefault = Boolean.valueOf(nodeValues.get("default"));
         this.visible = Boolean.valueOf(nodeValues.get("visible"));
+        this.personAccountDefault = Boolean.valueOf(nodeValues.get("personAccountDefault"));
     }
 
     @Override
     public Element getElement(ElementBuilder builder) {
-        return builder.createElement(getNodeName())
-                .addChild("default", isDefault)
-                .addChild("recordType", recordType)
+        builder.createElement(getNodeName())
+                .addChild("default", isDefault);
+
+        if (personAccountDefault) {
+            builder.addChild("personAccountDefault", personAccountDefault);
+        }
+
+        return builder.addChild("recordType", recordType)
                 .addChild("visible", visible)
                 .getElement();
     }
